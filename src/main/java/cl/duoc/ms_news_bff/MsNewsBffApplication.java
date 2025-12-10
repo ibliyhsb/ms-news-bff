e55f34c1b3fe3d09f404c1218cb0c070d9463882
ibliyhsb <catd.rojas@duocuc.cl>
2025-12-10 02:30:49 -0300

    proyecto base

---
+++ src/main/java/cl/duoc/ms_news_bff/MsNewsBffApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms_news_bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsNewsBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsNewsBffApplication.class, args);
+	}
+
+}
